2aeadcdd55b828a04a7e729728ecf8c013cdfde5
Lud0ca <locampol1800@alumno.ipn.mx>
2023-04-10 23:48:00 -0600

    Proyecto fundado Initializr

---
+++ src/test/java/com/EcomJava/ecommerceSpringBootBase/EcommerceSpringBootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EcomJava.ecommerceSpringBootBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringBootBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
